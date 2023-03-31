d9cf78f88a7cb756de73077838fa26af157b0239
Sean Brickley <sean@wabr.io>
2023-03-30 20:14:58 -0400

    feat: scheduler service + worker MVP

--- README.md
+++ README.md
@@ -1,1 +1,126 @@
-This is an empty repository
+# FizzBuzz Task Scheduler
+
+This project contains two binaries:
+
+- `server`, which exposes an HTTP API that allows a user to schedule tasks
+- `worker`, which pulls those scheduled tasks from a datastore and executes them at the specified time (or close enough to make little difference).
+
+## Using the task scheduling service
+
+### Create the database
+
+Run the following docker command:
+
+```
+docker run --rm --name fizzbuzz-task-db \
+	-e POSTGRES_HOST_AUTH_METHOD=trust \
+	-e POSTGRES_DB=fizzbuzz-task-db \
+	-p 5432:5432 postgres:15
+```
+
+This will create a suitable PostgreSQL instance for the scheduler service to talk to. You can also run the `start-pg` bash script in the repo root to do the same thing.
+
+
+Then run this `psql` command to create the table that the service will use:
+
+```
+psql --host=localhost -U postgres \
+	-c 'CREATE TABLE tasks (
+		id SERIAL PRIMARY KEY,
+		tasktype VARCHAR NOT NULL,
+		status VARCHAR NOT NULL,
+		time TIMESTAMPTZ NOT NULL
+	);'
+```
+
+You can also run the `init-db` bash script in the repo root.
+
+_Note: the `sqlx` library that the scheduler uses to talk to postgres ostensibly has tools that handle table creation for you on start up. I could not get these to work as advertised, so I resorted to doing it the old fashioned way and storing the commands in the bash scripts mentioned above._
+
+Once the postgres instance is running and has the appropriate tables, we can move on to compiling and running the server.
+
+### Running the server
+
+To start the server, `cd` into the project root and run `cargo run --bin server`. The server will launch, and start listening for requests on port `8000`
+
+The server exposes an API that allows users to:
+
+- Create new tasks
+- List all tasks in the database, optionally filtering by task type and task completion status
+- Look up a task by its ID
+- Delete a task by its ID
+
+It also exposes a few other endpoints that are only intended to be used by workers. These allow workers to:
+
+- Check the timestamp of the next task to be executed
+- Pull a task off the queue
+- Mark a task as completed
+
+### Creating new tasks
+
+Once the server is running, you can run an HTTP POST request using curl to schedule a new task:
+
+`curl -X POST -H 'content-type: application/json' -d '{"type": "Fizz", "time": "2023-03-28T11:59:59Z"}' localhost:8000/tasks`
+
+Valid inputs for the JSON payload are as folows:
+
+- `type` can either be `Fizz`, `Buzz`, or `FizzBuzz` (capitilization matters in this case)
+- `time` can be any valid RFC3339 formatted timestamp. All times are interpreted as UTC.
+
+A successful request should yield the task ID number.
+
+### Listing all tasks
+
+To view all known tasks, you can run an HTTP GET request like so:
+
+`curl -X GET 'localhost:8000/tasks'`
+
+This will yield a JSON payload detailing all tasks that the scheduler knows about. You can also add filters as query string parameters.
+
+For example, to view all tasks of type `"Fizz"` that have not been executed yet, you can run the following curl command:
+
+`curl -X GET 'localhost:8000/tasks?filters.type=fizz&filters.status=waiting'`
+
+Valid arguments for the task filters are as follows:
+- `filters.type` can be one of `fizz`, `buzz`, or `fizzbuzz` (capitalization does not matter in this case)
+- `filters.status` can be either `waiting`, `claimed`, or `complete`
+
+Both filters are optional. You can run your request with zero, one, or both filters set.
+
+### Getting a task by its ID
+
+To lookup a specific task by its task ID number, run the following `curl` command:
+`curl -X GET localhost:8000/tasks/<id>`
+
+Any positive integer is a valid ID number.
+
+This endpoint will return the JSON data for the task.
+
+### Deleting a task by its ID
+
+To delete a task, you can run a similar `curl` command using the DELETE method:
+
+`curl -X DELETE localhost:8000/tasks/<id>`
+
+This endpoint will return the JSON data for the deleted task.
+
+## Running the worker binary
+
+To run a worker, `cd` into the project root and run the following command:
+
+`cargo run --bin worker`
+
+This will run a worker process that will periodically check for any scheduled tasks that need to be run.
+
+- `Fizz` tasks will cause the worker to sleep for 3 seconds, starting at the scheduled execution time, before printing out the word `Fizz`, followed by the task ID number.
+- `Buzz` tasks will cause the worker to sleep for 5 seconds, starting at the scheduled execution time, before printing out the word `Buzz`, followed by the task ID number.
+- `FizzBuzz` tasks will run immediately once the scheduled execution time is reached or passed, before printing out the word `FizzBuzz`, followed by the current time.
+
+Worker processes can run in parallel with one another, each pulling tasksk off the queue and running them at the specified execution time.
+
+Each worker follows these steps:
+
+1. Check the timestamp for the task scheduled to run next. If there are no tasks currently on the queue, defer (i.e., sleep for 1 second, and go back to step 1)
+2. If it's not yet time to run the task, defer. Otheriwse, tell the server to return the task info and mark the task as claimed. 
+3. Run the task according to the task type.
+4. Once the task is done running, tell the server to mark the task as complete.
---
+++ src/bin/server.rs
@@ -0,0 +1,229 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::status::Created;
+use rocket::serde::json::Json;
+use rocket_db_pools::{sqlx, Connection, Database};
+
+use fizzbuzz::{
+	CreateTaskRequest, CreateTaskResponse, Filters, NextTaskTimeResponse, Task,
+	TaskID, TaskState,
+};
+
+type Result<T, E = rocket::response::Debug<sqlx::Error>> =
+	std::result::Result<T, E>;
+
+#[derive(Database)]
+#[database("fizzbuzz-task-db")]
+struct TaskDB(sqlx::PgPool);
+
+#[launch]
+async fn launch() -> _ {
+	rocket::build().attach(TaskDB::init()).mount(
+		"/tasks",
+		routes![
+			create_task,
+			list_tasks,
+			get_task,
+			delete_task,
+			get_next_task_time,
+			claim_next_task,
+			complete_task,
+		],
+	)
+}
+
+#[post("/", format = "application/json", data = "<task>")]
+async fn create_task(
+	mut db: Connection<TaskDB>,
+	task: Json<CreateTaskRequest>,
+) -> Result<Created<Json<CreateTaskResponse>>> {
+	let req = task.into_inner();
+	let record = sqlx::query!(
+		"INSERT INTO tasks (tasktype, time, status) VALUES ($1, $2, $3) RETURNING id",
+		req.typ.to_string(),
+		req.time,
+		TaskState::Waiting.to_string(),
+	)
+	.fetch_one(&mut *db)
+	.await?;
+
+	let resp = Json(CreateTaskResponse { id: record.id });
+	Ok(Created::new("/tasks").body(resp))
+}
+
+#[get("/<id>")]
+async fn get_task(
+	mut db: Connection<TaskDB>,
+	id: TaskID,
+) -> Result<Option<Json<Task>>> {
+	let r = sqlx::query!("SELECT * from tasks where id = $1", id)
+		.fetch_optional(&mut *db)
+		.await?;
+
+	match r {
+		Some(task) => Ok(Some(Json(Task {
+			typ: task.tasktype.clone().try_into().unwrap(),
+			status: task.status.clone().try_into().unwrap(),
+			time: task.time,
+			id: task.id,
+		}))),
+		None => Ok(None),
+	}
+}
+
+#[delete("/<id>")]
+async fn delete_task(
+	mut db: Connection<TaskDB>,
+	id: TaskID,
+) -> Result<Option<()>> {
+	sqlx::query!("DELETE from tasks where id = $1", id)
+		.execute(&mut *db)
+		.await?;
+
+	Ok(Some(()))
+}
+
+#[get("/next")]
+async fn get_next_task_time(
+	mut db: Connection<TaskDB>,
+) -> Result<Option<Json<NextTaskTimeResponse>>> {
+	let r = sqlx::query!("SELECT id, time FROM tasks WHERE status = 'Waiting' ORDER BY time, id LIMIT 1 ")
+	.fetch_optional(&mut *db)
+	.await?;
+
+	match r {
+		Some(task) => Ok(Some(Json(NextTaskTimeResponse { time: task.time }))),
+		None => Ok(None),
+	}
+}
+
+#[post("/next")]
+async fn claim_next_task(
+	mut db: Connection<TaskDB>,
+) -> Result<Option<Json<Task>>> {
+	let r = sqlx::query!(
+		r#"
+UPDATE tasks
+SET status = 'Claimed'
+WHERE id IN (SELECT id FROM tasks
+	WHERE status = 'Waiting' 
+	ORDER BY time, id
+	LIMIT 1)
+RETURNING id, tasktype, time, status
+		"#,
+	)
+	.fetch_optional(&mut *db)
+	.await?;
+
+	match r {
+		Some(task) => Ok(Some(Json(Task {
+			typ: task.tasktype.clone().try_into().unwrap(),
+			status: task.status.clone().try_into().unwrap(),
+			time: task.time,
+			id: task.id,
+		}))),
+		None => Ok(None),
+	}
+}
+
+#[post("/<id>")]
+async fn complete_task(
+	mut db: Connection<TaskDB>,
+	id: TaskID,
+) -> Result<Option<Json<Task>>> {
+	let r =
+		sqlx::query!("UPDATE tasks SET status = 'Complete' WHERE id = $1 AND status = 'Claimed' RETURNING *", id)
+			.fetch_optional(&mut *db)
+			.await?;
+
+	match r {
+		Some(task) => Ok(Some(Json(Task {
+			typ: task.tasktype.clone().try_into().unwrap(),
+			status: task.status.clone().try_into().unwrap(),
+			time: task.time,
+			id: task.id,
+		}))),
+		None => Ok(None),
+	}
+}
+
+// This function is more repetitive than I would have liked, particular when it comes to
+// the closure passed into each `map` call. It would be nice to turn that into a function
+// that I can call whenever I need to convert a record into a task. Unfortunately, the type
+// of `record` is opaque here.
+//
+// Writing a function that takes a string and passing that to `query!()` would also work.
+// Unfortunately, that macro expects a string literal, and not a variable name, for some reason.
+//
+// Long story short, this code is ugly and repetitive, but it will have to do for now.
+#[get("/?<filters>")]
+async fn list_tasks(
+	mut db: Connection<TaskDB>,
+	filters: Filters,
+) -> Result<Json<Vec<Task>>> {
+	let tasks: Vec<Task> = match (filters.type_filter, filters.status_filter) {
+		// No filter
+		(None, None) => sqlx::query!("SELECT * FROM tasks")
+			.fetch_all(&mut *db)
+			.await?
+			.iter()
+			.map(|record| Task {
+				typ: record.tasktype.clone().try_into().unwrap(),
+				status: record.status.clone().try_into().unwrap(),
+				time: record.time,
+				id: record.id,
+			})
+			.collect(),
+
+		// Type filter
+		(Some(t), None) => sqlx::query!(
+			"SELECT * FROM tasks WHERE tasktype = $1",
+			t.to_string()
+		)
+		.fetch_all(&mut *db)
+		.await?
+		.iter()
+		.map(|record| Task {
+			typ: record.tasktype.clone().try_into().unwrap(),
+			status: record.status.clone().try_into().unwrap(),
+			time: record.time,
+			id: record.id,
+		})
+		.collect(),
+
+		// Status filter
+		(None, Some(s)) => {
+			sqlx::query!("SELECT * FROM tasks WHERE status = $1", s.to_string())
+				.fetch_all(&mut *db)
+				.await?
+				.iter()
+				.map(|record| Task {
+					typ: record.tasktype.clone().try_into().unwrap(),
+					status: record.status.clone().try_into().unwrap(),
+					time: record.time,
+					id: record.id,
+				})
+				.collect()
+		}
+
+		// Type and status filters
+		(Some(t), Some(s)) => sqlx::query!(
+			"SELECT * FROM tasks WHERE tasktype = $1 AND status = $2",
+			t.to_string(),
+			s.to_string(),
+		)
+		.fetch_all(&mut *db)
+		.await?
+		.iter()
+		.map(|record| Task {
+			typ: record.tasktype.clone().try_into().unwrap(),
+			status: record.status.clone().try_into().unwrap(),
+			time: record.time,
+			id: record.id,
+		})
+		.collect(),
+	};
+
+	Ok(Json(tasks))
+}
